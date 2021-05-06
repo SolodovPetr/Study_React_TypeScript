a1e216c4d9ae7b2c503dfabe244f5c4e646d4b39
Petr <solodov.petr@gmail.com>
2021-05-06 10:45:56 +0300

    onDragStart event example

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 
 import GuestList from './state/GuestList';
 import UserSearch from './state/UserSearch';
+import EventComponent from './events/EventComponent';
 
 const App = () => {
   return (
     <div>
       <UserSearch />
       <GuestList />
+      <EventComponent />
     </div>
   );
 };
