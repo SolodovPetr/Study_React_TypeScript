84d8eb8fe83dcd3b7bc82cda5e1c1547d3558323
Petr <solodov.petr@gmail.com>
2021-05-06 17:39:48 +0300

    Dispatch actionCreators on submit

--- src/redux-ts/src/components/RepositoriesList.tsx
+++ src/redux-ts/src/components/RepositoriesList.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../state';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
+  const dispatch = useDispatch();
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    dispatch(actionCreators.searchRepositories(term));
   };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
